3a7780f7bdb5c79a2ef76d24b1eced4616321c69
matheusDrumond <drumond.matheusc@gmail.com>
2023-06-29 21:01:38 -0300

    Reformulação no HTML e JS

--- src/index.js
+++ src/index.js
@@ -1,33 +1,41 @@
 'use strict'
 
-//delcaraçao dos campos do formulário de rendimento
-const valorInicial   = document.querySelector('#valorInicial').value;
-const taxaDeJuros    = document.querySelector('#juros').value;
-const porMes         = document.querySelector('#p-month').value;
-const porAno         = document.querySelector('#p-year').value;
-const tipoJuros      = document.querySelector('#type').value;
-const tempo          = document.querySelector('#time').value;
+let botaoCalcular = document.querySelector('#calcular');
 
+// botaoCalcular.addEventListener('click', ()=>{
+//     let valor = document.querySelector('#valorInicial').value;
+//     let taxa = document.querySelector('#juros').value;
+//     let periodo = document.querySelector('#periodo').value;
+//     let tipo = document.querySelector('#type').value;
+//     let tempo = document.querySelector('#time').value;
+//     let tipoTempo = document.querySelector('#time-type').value;
+//     console.log(valor);
+//     console.log(taxa);
+//     console.log(periodo);
+//     console.log(tipo);
+//     console.log(tempo);
+//     console.log(tipoTempo);
+// })
 
-// Cálculo de juros simples
+//funções 
 function jurosSimples(valor, taxa, time){
     let juros = (valor * (taxa / 100) * time).toFixed(2);
     let montante = (valor + parseFloat(juros)).toFixed(2);
-
-    let resultado = document.querySelector('#resultado');
-    resultado.textContent = montante;
-    resultado.classList.remove('text-white');
-    resultado.classList.add('text-black');
-
-    // teste: console.log(jurosSimples(300, 5, 12));
+    return `O total de juros foi ${juros} e o montante final é ${montante}`;
 }
 
 
-// Cálculo de juros compostos 
 function jurosCompostos(valor, taxa, time){
     let juros = (valor * Math.pow(1 + (taxa / 100), time) - valor).toFixed(2);
     let montante = (valor + parseFloat(juros)).toFixed(2);
     return `O total de juros foi ${juros} e o montante final é ${montante}`;
-    //console.log(jurosCompostos(300, 5, 12));
 }
+
+
+
+// juros simples
+// if(document.querySelector('#type').value == 'simples' 
+// && document.querySelector('#periodo').value == 'p-month'
+// && document.querySelector('#time-type').value == 'month'){
+// }
 
