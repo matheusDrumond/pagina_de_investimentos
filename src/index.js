3b890c3df56faa0ab9fc066f97cab4f7f7316130
matheusDrumond <drumond.matheusc@gmail.com>
2023-07-01 11:17:00 -0300

    Funcionalidades da página de rendimentos

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
 'use strict'
 
-let botaoCalcular = document.querySelector('#calcular');
+// Página de rendimentos
 
-// botaoCalcular.addEventListener('click', ()=>{
-    // let valor = document.querySelector('#valorInicial').value;
-    // let taxa = document.querySelector('#juros').value;
-//     let periodo = document.querySelector('#periodo').value;
-//     let tipo = document.querySelector('#type').value;
-//     let tempo = document.querySelector('#time').value;
-//     let tipoTempo = document.querySelector('#time-type').value;
-//     console.log(valor);
-//     console.log(taxa);
-//     console.log(periodo);
-//     console.log(tipo);
-//     console.log(tempo);
-//     console.log(tipoTempo);
-// })
-
-//funções 
+// Funções 
 function jurosSimples(valor, taxa, tempo) {
     let juros = (valor * (taxa / 100) * tempo).toFixed(2)
     let montante = parseFloat(valor) + parseFloat(juros);
-    let mensagem = `O total de juros foi ${juros} e o montante final é ${montante}`;
+    let mensagem = `O total de juros foi R$${juros} e o montante final é R$${montante}`;
   
     let resultado = document.querySelector('#resultado');
     resultado.innerHTML = mensagem;
     resultado.classList.replace('text-white', 'text-green-500');
   }
-  
-  // Wait for the DOM to be loaded before accessing the elements
-  
-  
-
 
 function jurosCompostos(valor, taxa, time){
     let juros = (valor * Math.pow(1 + (taxa / 100), time) - valor).toFixed(2);
@@ -44,12 +24,16 @@ function jurosCompostos(valor, taxa, time){
 }
 
 
-//lógica dos cálculos
+// Lógica dos cálculos
+
+// Espera do DOM carregar o conteúdo
 document.addEventListener('DOMContentLoaded', function() {
     let botaoCalcular = document.querySelector('#calcular');
   
     botaoCalcular.addEventListener('click', function() {
-      //juros simples
+      // Juros simples
+
+      // Tudo por mês
       if (
         document.querySelector('#periodo').value === 'p-month' &&
         document.querySelector('#type').value === 'simples' &&
@@ -60,7 +44,41 @@ document.addEventListener('DOMContentLoaded', function() {
         let tempo = document.querySelector('#time').value;
         jurosSimples(valor, taxa, tempo);
       }
-      //juros compostos
+      // Tudo por ano
+      else if(
+        document.querySelector('#periodo').value === 'p-year' &&
+        document.querySelector('#type').value === 'simples' &&
+        document.querySelector('#time-type').value === 'year'
+      ){
+        let valor = document.querySelector('#valorInicial').value;
+        let taxa = document.querySelector('#juros').value;
+        let tempo = document.querySelector('#time').value;
+        jurosSimples(valor, taxa, tempo);
+      }
+      // Juros por mês e tempo em anos
+      else if(
+        document.querySelector('#periodo').value === 'p-month' &&
+        document.querySelector('#type').value === 'simples' &&
+        document.querySelector('#time-type').value === 'year'
+      ){
+        let valor = document.querySelector('#valorInicial').value;
+        let taxa = document.querySelector('#juros').value * 12;
+        let tempo = document.querySelector('#time').value;
+        jurosSimples(valor, taxa, tempo);
+      }
+      // Juros por ano e tempo em meses
+      else if(
+        document.querySelector('#periodo').value === 'p-year' &&
+        document.querySelector('#type').value === 'simples' &&
+        document.querySelector('#time-type').value === 'month'
+      ){
+        let valor = document.querySelector('#valorInicial').value;
+        let taxa = document.querySelector('#juros').value;
+        let tempo = document.querySelector('#time').value / 12;
+        jurosSimples(valor, taxa, tempo);
+      }
+      // Juros compostos
+      // Tudo em meses
       else if(
         document.querySelector('#periodo').value === 'p-month' &&
         document.querySelector('#type').value === 'composto' &&
@@ -70,6 +88,39 @@ document.addEventListener('DOMContentLoaded', function() {
         let taxa = document.querySelector('#juros').value;
         let tempo = document.querySelector('#time').value;
         jurosCompostos(valor, taxa, tempo)
+      }
+      // Tudo por ano
+      else if(
+        document.querySelector('#periodo').value === 'p-year' &&
+        document.querySelector('#type').value === 'composto' &&
+        document.querySelector('#time-type').value === 'year'
+      ){
+        let valor = document.querySelector('#valorInicial').value;
+        let taxa = document.querySelector('#juros').value;
+        let tempo = document.querySelector('#time').value;
+        jurosSimples(valor, taxa, tempo);
+      }
+      // Juros por mês e tempo em anos
+      else if(
+        document.querySelector('#periodo').value === 'p-month' &&
+        document.querySelector('#type').value === 'composto' &&
+        document.querySelector('#time-type').value === 'year'
+      ){
+        let valor = document.querySelector('#valorInicial').value;
+        let taxa = document.querySelector('#juros').value * 12;
+        let tempo = document.querySelector('#time').value;
+        jurosSimples(valor, taxa, tempo);
+      }
+      // Juros por ano e tempo em meses
+      else if(
+        document.querySelector('#periodo').value === 'p-year' &&
+        document.querySelector('#type').value === 'composto' &&
+        document.querySelector('#time-type').value === 'month'
+      ){
+        let valor = document.querySelector('#valorInicial').value;
+        let taxa = document.querySelector('#juros').value;
+        let tempo = document.querySelector('#time').value / 12;
+        jurosSimples(valor, taxa, tempo);
       }
     });
   });
