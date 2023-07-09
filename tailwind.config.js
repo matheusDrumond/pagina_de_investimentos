c22217370990404081d1766e2f70b5ba46bc05bb
matheusDrumond <drumond.matheusc@gmail.com>
2023-07-09 19:30:46 -0300

    Responsividade

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,27 @@ module.exports = {
   content: ['./src/**/*{html, js}'],
   theme: {
     extend: {
-      '3xl': '0 35px 35px rgba(0, 0, 0, 0.6)'
+      dropShadow:{
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.6)',
+      },
+      keyframes: {
+        upDown:{
+          '0%': {},
+          '33%': {transform: 'translateY(2em)'},
+          '66': {trasform: 'translateY(-4em)'},
+          '100%': {translate: 'translateY(4em)'}
+        }
+      },
+      animation: {
+        upDown: 'upDown 10s ease-in-out infinite'
+      },
+      fontFamily: {
+        'Oswald': ['Oswald', 'sans-serif'] 
+      },
+      screens:{
+        'phone': "320px"
+      },
     },
-    keyframes: {
-      upDown:{
-        '0%': {},
-        '33%': {transform: 'translateY(2em)'},
-        '66': {trasform: 'translateY(-4em)'},
-        '100%': {translate: 'translateY(4em)'}
-      }
-    },
-    animation: {
-      upDown: 'upDown 10s ease-in-out infinite'
-    },
-    fontFamily: {
-      'Oswald': ['Oswald', 'sans-serif'] 
-    },
-    screens:{
-      'phone': "320px"
-    }
   },
   plugins: [],
 }
